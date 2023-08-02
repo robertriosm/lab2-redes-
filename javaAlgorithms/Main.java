a95f6dacc23ede138755e214277f8579079e5d25
robertriosm <87707371+robertriosm@users.noreply.github.com>
2023-08-01 20:59:04 -0600

    fixed

--- javaAlgorithms/Main.java
+++ javaAlgorithms/Main.java
@@ -1,4 +1,4 @@
-package java;
+package javaAlgorithms;
 
 public class Main {
     public static void main(String[] args) {
---
+++ javaAlgorithms/Vitervi.java
@@ -0,0 +1,46 @@
+package javaAlgorithms;
+
+
+/**
+ * vitervi
+ */
+public class Vitervi {
+
+    /**
+     * attrs
+     */
+    public int x;
+    public int y;
+
+    /*
+     * constructor
+     */
+    public Vitervi(int x, int y) {
+        this.x = x;        
+        this.y = y;
+    }
+
+    /*
+     * getters
+     */
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    /*
+     * setters
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    
+}
