b0586548ddb736adc06a7615f294e8720aff284a
Pedro Arriola <pedroparty310@gmail.com>
2023-08-03 00:13:24 -0600

    :gear: - Se ha implementado el algoritmo Fletcher Checksum en Python y Java

---
+++ java_algorithms/Vitervi.java
@@ -0,0 +1,25 @@
+package java_algorithms;
+/**
+ * vitervi
+ */
+public class Vitervi {
+
+    /**
+     * attrs
+     */
+    public int x;
+    public int y;
+
+    /*
+     * constructor
+     */
+    public Vitervi(int x, int y) {
+        this.x = x;        
+        this.y = y;
+
+
+    }
+
+
+    
+}
